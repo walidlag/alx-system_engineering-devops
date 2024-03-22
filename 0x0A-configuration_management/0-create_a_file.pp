b8f0051f17fbf2202e8cd2023dea2bd33eea3631
walidlag <walidlagmah8@gmail.com>
2024-03-22 14:06:34 -0700

    walidlag

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# creates a file in /tmp
+
+file { '/tmp/school':
+  content =>'I love Puppet',
+  mode    => '0744',
+  group   => 'www-data',
+  owner   => 'www-data',
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,8 @@
+# create a manifest
+
+# kill process killmenow
+
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
+}
